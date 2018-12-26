9c73478cef7bb3788b79932535690af924f450ca
Nemirtingas <nanaki89@hotmail.fr>
2018-12-26 10:07:57 +0100

    Added licence

--- pxa1908/gps_mrvl.cpp
+++ pxa1908/gps_mrvl.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <hardware/gps.h>
 #include <utils/Log.h>
 
@@ -74,6 +91,6 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .hal_api_version = 0,
     .id = GPS_HARDWARE_MODULE_ID,
     .name = "MRVL GPS Module",
-    .author = "Marvell",
+    .author = "Nemirtingas (Maxime P)",
     .methods = &gps_module_methods,
 };
